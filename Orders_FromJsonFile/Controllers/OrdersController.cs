48d13b8637ac6517ac20c994ea3949a457ed1244
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-20 01:17:35 -0500

    Add controller and NewtonsoftJson service

---
+++ Orders_FromJsonFile/Controllers/OrdersController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Orders_FromJsonFile.Models;
+
+namespace Orders_FromJsonFile.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+
+
+        [HttpGet("sum")]
+        public IActionResult GetOrderSums()
+        {
+            var filePath = "JsonFiles/OrdersFile.json";
+            var jsonData = System.IO.File.ReadAllText(filePath);
+            var customers = JsonConvert.DeserializeObject<List<Customer>>(jsonData);
+
+            var orderSums = customers.Select(c => new
+            {
+                CustomerId = c.Id,
+                OrderSums = c.Orders.Select(o => new
+                {
+                    OrderId = o.Id,
+                    Sum = o.Details.Sum(d => d.Quantity)
+                })
+            });
+
+            return Ok(orderSums);
+        }
+    }
+}
