3d3c575320c4cf3ce8c9ca168f950073378d8656
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-20 00:55:00 -0500

    Add OrderDetail model

---
+++ Orders_FromJsonFile/Models/OrderDetail.cs
@@ -0,0 +1,9 @@
+﻿namespace Orders_FromJsonFile.Models
+{
+    public class OrderDetail
+    {
+        public string DatilId { get; set; } = default!;
+        public int PluId { get; set; }
+        public int Quantity { get; set; }
+    }
+}
